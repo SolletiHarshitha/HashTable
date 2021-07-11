d41814129f9e47ee7cb316100ffa3902e13615ba
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-11 23:37:33 +0530

    [Harshitha] Ability to find frequency of words in a sentence

--- HashTableImplementation/MyMapNode.cs
+++ HashTableImplementation/MyMapNode.cs
@@ -11,8 +11,8 @@ namespace HashTableImplementation
             public k Key { get; set; }
             public v Value { get; set; }
         }
-        private readonly int size;
-        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public int size;
+        public LinkedList<KeyValue<K, V>>[] items;
 
         public MyMapNode(int size)
         {
@@ -49,6 +49,20 @@ namespace HashTableImplementation
             { Key = key, Value = value } ;
             linkedList.AddLast(item);
             Console.WriteLine(item.Key+" "+item.Value);
-        } 
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
     }
 }
--- HashTableImplementation/Program.cs
+++ HashTableImplementation/Program.cs
@@ -7,13 +7,18 @@ namespace HashTableImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("---------------------Hash Table-----------------------");
-            MyMapNode<int, string> hashTable = new MyMapNode<int, string>(6);
-            hashTable.Add(0, "To");
-            hashTable.Add(1, "be");
-            hashTable.Add(2, "or");
-            hashTable.Add(3, "not");
-            hashTable.Add(4, "to");
-            hashTable.Add(5, "be");
+            string sentence = "To be or not to be";
+            string[] words = sentence.Split(' ');
+            MyMapNode<int, string> hashTable = new MyMapNode<int, string>(words.Length);
+            int key = 0;
+            foreach(string word in words)
+            {
+                hashTable.Add(key, word);
+                key++;
+            }
+       
+            Operation operation = new Operation();
+            operation.Frequency(hashTable);
         }
     }
 }
